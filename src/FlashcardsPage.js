7d14ea2910439f391a7b4c3b8857b7aa5b9dd6da
= <Jordan.yn.chu@gmail.com>
2024-08-03 18:42:14 -0400

    feat: created flashcard boxes

--- src/FlashcardsPage.js
+++ src/FlashcardsPage.js
@@ -1,8 +1,29 @@
+var decks = [
+  { id: 1, name: "Spanish", cards: 10, lastVisited: 100},
+  { id: 2, name: "French", cards: 43, lastVisited: 0}
+]
+
+
+const Decks = () => (
+  decks.map(decks => (
+    <div className="block p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadowblock">
+      <h3 className="text-lg text-center">{decks.name}</h3>
+      <div>
+        <span className="float-left text-gray-600">{decks.cards}</span>
+        <span className="float-right text-gray-600">{decks.lastVisited}</span>
+      </div>
+    </div>
+  ))
+)
 
 const FlashcardsPage = () => (
+
+
   <div className="p-6">
-    <h2 className="text-2xl font-bold mb-4">Welcome to the Language App</h2>
-    <p>Flashcards</p>
+    <h2 className="text-2xl font-bold mb-4">Flashcard Title</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Decks />
+    </div>
   </div>
 );
 
